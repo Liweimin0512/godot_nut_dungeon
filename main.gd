d3b7209b1aa2159dcde50a85bfef33b1ac2f8e91
2410250284@qq.com <2410250284@qq.com>
2024-12-20 18:59:55 +0800

    拙劣的表现层

--- main.gd
+++ main.gd
@@ -1,17 +1,60 @@
 extends Node2D
 
+@onready var rich_text_label: RichTextLabel = $CanvasLayer/RichTextLabel
+
 @export var max_turn_count: int = 99
 var _combat_characters : Array[Character]
 var _turn_count : int = 0
 
+signal combat_started
+signal turn_started
+signal turn_ended
+signal combat_finished
+signal combat_defeated
+
 func _ready() -> void:
+	combat_started.connect(
+		func() -> void:
+			rich_text_label.text += "战斗开始\n"
+	)
+	turn_started.connect(
+		func(turn_count: int) -> void:
+			rich_text_label.text += "{0}回合开始\n".format([
+				turn_count,
+			])
+	)
+	turn_ended.connect(
+		func() -> void:
+			rich_text_label.text += "回合结束\n"
+	)
+	combat_finished.connect(
+		func() -> void:
+			rich_text_label.text += "战斗胜利\n"
+	)
+	combat_defeated.connect(
+		func() -> void:
+			rich_text_label.text += "战斗失败\n"
+	)
 	_combat_start()
 
 ## 战斗开始
 func _combat_start() -> void:
 	print("战斗开始！")
-	for cha in get_tree().get_nodes_in_group("Character"):
+	combat_started.emit()
+	for cha : Character in get_tree().get_nodes_in_group("Character"):
 		_combat_characters.append(cha as Character)
+		cha.hited.connect(
+			func(target: Character) -> void:
+				rich_text_label.text += "{0} 攻击 {1}\n".format([
+					cha, target
+				])
+		)
+		cha.hurted.connect(
+			func(damage: int) -> void:
+				rich_text_label.text += "{0} 受到{1}点伤害！\n".format([
+					cha, damage
+				])
+		)
 	for character in _combat_characters:
 		character.combat_start()
 	_turn_start()
@@ -20,15 +63,17 @@ func _combat_start() -> void:
 func _turn_start() -> void:
 	_turn_count += 1
 	print(_turn_count, "回合开始===================")
+	turn_started.emit(_turn_count)
 	_combat_characters.shuffle()
 	_combat_characters.sort_custom(
 		func(a: Character, b: Character) -> bool:
+			if not a or not b: return false
 			return a.speed > b.speed
 	)
 	for character in _combat_characters:
 		if not is_instance_valid(character): continue
 		if not character.is_alive: continue
-		character.turn_start()
+		await character.turn_start()
 	_turn_end()
 
 ## 回合结束
@@ -42,18 +87,24 @@ func _turn_end() -> void:
 			print("回合数耗尽")
 			_combat_defeat()
 		for character in _combat_characters:
+			if not is_instance_valid(character): continue
 			character.turn_end()
 		print("回合结束===================")
+		turn_ended.emit()
 		_turn_start()
 
 ## 战斗胜利
 func _combat_finish() -> void:
 	for cha in _combat_characters:
+		if not cha : continue
 		cha.combat_end()
 	print("===== 战斗胜利 =====, 回合数： ", _turn_count)
+	combat_finished.emit()
 
 ## 战斗失败
 func _combat_defeat() -> void:
 	for cha in _combat_characters:
+		if not cha : continue
 		cha.combat_end()
 	print("===== 战斗失败 =====, 回合数： ", _turn_count)
+	combat_defeated.emit()
--- source/character.gd
+++ source/character.gd
@@ -3,8 +3,18 @@ class_name Character
 
 ## 战斗角色
 
+@onready var label_name: Label = %LabelName
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var progress_bar: ProgressBar = $MarginContainer/ProgressBar
+@onready var label_health: Label = %LabelHealth
+@onready var label_action: Label = %LabelAction
+
 @export var max_health : float = 100
-@export_storage var _current_health : float = 100
+@export_storage var _current_health : float = 100:
+	set(value):
+		_current_health = value
+		progress_bar.value = _current_health
+		label_health.text = "{0}/{1}".format([_current_health,max_health ])
 @export var attack_power : float = 10.0
 @export var defense_power : float = 5
 @export var speed : float = 1
@@ -15,8 +25,24 @@ var is_alive : bool :
 	get:
 		return _current_health > 0
 
+signal hited(target: Character)
+signal hurted(damage: int)
+
 func _ready() -> void:
 	_current_health = max_health
+	label_name.text = cha_name
+	hited.connect(
+		func(target: Character) -> void:
+			label_action.text = "攻击{0}".format([target])
+			animation_player.play("hit")
+	)
+	hurted.connect(
+		func(_damage) -> void:
+			animation_player.play("hurt")
+	)
+	progress_bar.max_value = max_health
+	progress_bar.value = _current_health
+	label_health.text = "{0}/{1}".format([_current_health,max_health ])
 
 ## 战斗开始
 func combat_start() -> void:
@@ -26,7 +52,8 @@ func combat_start() -> void:
 func turn_start() -> void:
 	print(self, "====== 回合开始")
 	var target := _get_target()
-	hit(target)
+	await get_tree().create_timer(0.5).timeout
+	await hit(target)
 
 ## 回合结束
 func turn_end() -> void:
@@ -39,12 +66,16 @@ func combat_end() -> void:
 ## 攻击
 func hit(target: Character) -> void:
 	if not target: return
+	hited.emit(target)
+	await get_tree().create_timer(0.5).timeout
+	if not target: return
 	print(self, " 攻击： ", target)
-	var damage := attack_power - target.defense_power
-	target.hurt(damage)
+	var damage : int = attack_power - target.defense_power
+	await target.hurt(damage)
 
 ## 受击
-func hurt(damage) -> void:
+func hurt(damage: int) -> void:
+	hurted.emit(damage)
 	print(self, " 受到伤害： ", damage)
 	_current_health -= damage
 	_current_health = max(_current_health, 0)
@@ -54,6 +85,9 @@ func hurt(damage) -> void:
 ## 死亡
 func _die() -> void:
 	print("角色死亡：", self)
+	animation_player.play("die")
+	if not is_inside_tree(): return
+	await get_tree().create_timer(1).timeout
 	get_parent().remove_child(self)
 	queue_free()
 
