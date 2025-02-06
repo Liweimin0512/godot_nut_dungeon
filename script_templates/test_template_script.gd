b118ae2836810d93fb2c8cee9459f61ff89015bb
OldLee <2410250284@qq.com>
2025-02-06 13:54:35 +0800

    自定义Object脚本模板

---
+++ script_templates/test_template_script.gd
@@ -0,0 +1,38 @@
+extends Node
+
+#region 常量和信号
+#endregion
+
+#region Export变量
+#region
+
+#region ONREADY变量
+#region
+
+#region 共有变量
+#endregion
+
+#region 私有变量
+#endregion
+
+#region 生命周期函数
+
+func _ready() -> void:
+	pass 
+
+
+func _process(delta: float) -> void:
+	pass
+#endregion
+
+#region 公共方法
+#endregion
+
+#region 私有方法
+#endregion
+
+#region 信号回调
+#endregion
+
+#region 重写方法
+#endregion
