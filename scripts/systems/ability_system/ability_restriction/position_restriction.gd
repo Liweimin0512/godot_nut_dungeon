f728389de11564428fd55b5d25d21d6695589834
OldLee <2410250284@qq.com>
2025-02-21 14:44:03 +0800

    技能限制器业务层开发

--- scripts/systems/ability_system/ability_restriction/position_restriction.gd
+++ scripts/systems/ability_system/ability_restriction/position_restriction.gd
@@ -9,12 +9,12 @@ class_name PositionRestriction
 @export var target_positions: Array = [1, 2, 3, 4]
 
 func _init(config: Dictionary = {}) -> void:
-    valid_positions = config.get("valid_positions", [1, 2, 3, 4])
-    target_positions = config.get("target_positions", [1, 2, 3, 4])
+	valid_positions = config.get("valid_positions", [1, 2, 3, 4])
+	target_positions = config.get("target_positions", [1, 2, 3, 4])
 
 func can_use(context: Dictionary) -> bool:
-    var position = context.get("position", -1)
-    return _can_use_at_position(position)
+	var position = context.get("position", -1)
+	return _can_use_at_position(position)
 
 ## 判断能否在指定位置使用
 func _can_use_at_position(position: int) -> bool:
--- scripts/systems/ability_system/turn_based_skill_ability.gd
+++ scripts/systems/ability_system/turn_based_skill_ability.gd
@@ -17,13 +17,10 @@ enum TARGET_RANGE{
 @export var target_range: TARGET_RANGE = TARGET_RANGE.SELF
 ## 使用次数, 0代表不限次数
 @export var use_count: int = 1
-## 已经使用的次数
-@export_storage var available_count: int = 0
-## 剩余使用次数
-var left_count : int = 0:
-	get:
-		return use_count - available_count
 @export var is_melee : bool = false
+@export var valid_positions : Array
+@export var target_positions : Array
+var position_restriction : PositionRestriction
 
 func _init() -> void:
 	# 添加使用次数限制
@@ -32,17 +29,18 @@ func _init() -> void:
 	}))
 	
 	# 添加位置限制
-	add_restriction(PositionRestriction.new({
+	position_restriction = add_restriction(PositionRestriction.new({
 		"valid_positions": valid_positions,
 		"target_positions": target_positions
 	}))
+
 
 ## 获取可选择的目标位置
 func get_available_positions(position: int) -> Array:
 	if target_range == TARGET_RANGE.SELF:
 		return [position]
 	elif target_range == TARGET_RANGE.SINGLE_ENEMY or target_range == TARGET_RANGE.SINGLE_ALLY:
-		return target_positions
+		return position_restriction.target_positions
 	elif target_range == TARGET_RANGE.ALL_ENEMY or target_range == TARGET_RANGE.ALL_ALLY:
-		return valid_positions
-	return target_positions
+		return position_restriction.valid_positions
+	return position_restriction.target_positions
